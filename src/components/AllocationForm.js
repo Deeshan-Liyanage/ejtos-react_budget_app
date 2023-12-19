f6c639ff5274ad844f28e670f62b1d31d71269aa
Deeshan-Liyanage <2deeshanliyanage7@gmail.com>
2023-12-19 13:10:26 -0500

    Add new feature

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,6 +3,7 @@ import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
+    const {Currency} = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,19 +59,23 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
+                    <div>
+                    <label style={{ marginLeft: '2rem' }}>{Currency}</label>
+                        <input
+                            required='required'
+                            type='number'
+                            id='cost'
+                            value={cost}
+                            style={{ marginLeft: '0.5rem' }}
+                            onChange={(event) => setCost(event.target.value)}>
                         </input>
+                    </div>
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
+
+
                 </div>
                 </div>
 
